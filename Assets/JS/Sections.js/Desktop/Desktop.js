884573a84a30d4ccb1892647164720087befdd04
EstebanCantor <experto@koete.co>
2024-11-17 20:11:30 -0500

    Update Desktop.js

--- Assets/JS/Sections.js/Desktop/Desktop.js
+++ Assets/JS/Sections.js/Desktop/Desktop.js
@@ -377,3 +377,16 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 })();
     
+document.addEventListener('DOMContentLoaded', function() {
+    const leftButton = document.querySelector('#buttons-slide-desk .cta-button-left');
+    const rightButton = document.querySelector('#buttons-slide-desk .cta-button-right');
+
+    // Ejemplo de funcionalidad
+    leftButton.addEventListener('click', function() {
+        console.log('Botón izquierdo clicado');
+    });
+
+    rightButton.addEventListener('click', function() {
+        console.log('Botón derecho clicado');
+    });
+});
