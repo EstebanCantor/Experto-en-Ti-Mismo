ac2cf0bb144df6423988449461632dc6c0a06574
EstebanCantor <experto@koete.co>
2024-11-17 20:10:14 -0500

    Update Desktop.js
    Section-7

--- Assets/JS/Sections.js/Desktop/Desktop.js
+++ Assets/JS/Sections.js/Desktop/Desktop.js
@@ -295,3 +295,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.addEventListener('DOMContentLoaded', initCarousel);
 })();
+
+
+let currentImageIndexInteractive = 0;
+const totalImages = 2; // Número total de imágenes
+
+function updateImageInteractive() {
+    const imageContainer = document.querySelector('#section-interactive .image-container');
+    const offset = -currentImageIndexInteractive * 100;
+    imageContainer.style.transform = `translateX(${offset}%)`;
+}
+
+function showTextInteractive(index) {
+    const textsInteractive = document.querySelectorAll('#section-interactive .option-text p');
+    const optionsInteractive = document.querySelectorAll('#section-interactive .option');
+
+    textsInteractive.forEach((text, i) => {
+        text.style.display = i === index ? 'block' : 'none';
+    });
+
+    optionsInteractive.forEach((option, i) => {
+        option.classList.toggle('active', i === index);
+    });
+
+    currentImageIndexInteractive = index;
+    updateImageInteractive();
+}
+
+function prevImageInteractive() {
+    currentImageIndexInteractive = (currentImageIndexInteractive - 1 + totalImages) % totalImages;
+    updateImageInteractive();
+    showTextInteractive(currentImageIndexInteractive);
+}
+
+function nextImageInteractive() {
+    currentImageIndexInteractive = (currentImageIndexInteractive + 1) % totalImages;
+    updateImageInteractive();
+    showTextInteractive(currentImageIndexInteractive);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    showTextInteractive(0);
+});
