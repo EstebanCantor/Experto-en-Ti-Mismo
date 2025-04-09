10e760306faa4aafe4ae74e5aaedb5b9aa8a80b7
EstebanCantor <experto@koete.co>
2025-04-08 19:19:20 -0500

    Update Mobile.js
    dated update

--- Assets/JS/Sections.js/Mobile/Mobile.js
+++ Assets/JS/Sections.js/Mobile/Mobile.js
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const secondsElementMobile = document.querySelector('#section-mobile-1 #seconds-value');
             const countdownContainerMobile = document.querySelector('#section-mobile-1 .countdown');
 
-            const countDownDate = new Date("Mar 12, 2025 19:30:00").getTime();
+            const countDownDate = new Date("Apr 30, 2025 19:30:00").getTime();
             const countdownFunctionMobile = setInterval(function () {
                 const now = new Date().getTime();
                 const distance = countDownDate - now;
